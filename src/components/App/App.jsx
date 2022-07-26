a53fce07f213008c550d699f1f8ed1971aaae291
Sergey Chv <serge.techno@gmail.com>
2022-07-27 00:17:33 +0300

    added some styles, fixed base project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import './_vars.scss'
+import './_reset.scss'
+import './_base.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from react</h1>
+    </div>
+  );
+}
+
+export default App;
